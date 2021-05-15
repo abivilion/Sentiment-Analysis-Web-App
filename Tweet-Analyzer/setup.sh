0a1ac2b03b82ca33cc9afad11fcfec50b5819756
Ayush Bhardwaj <55326824+abivilion@users.noreply.github.com>
2021-05-15 23:53:08 +0530

    Update setup.sh

--- Tweet-Analyzer/setup.sh
+++ Tweet-Analyzer/setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"haritshandilya198@gmail.com\"\n\
+email = \"ayushbhardwaj6757@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
